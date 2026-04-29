d270bba15c1d544c6bb439ddb6c26fec04d0ebbd
Padee Vang <vangpadee11@gmail.com>
2026-04-28 19:30:16 -0500

    created log in page - testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,23 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
+import LogIn from './LogIn/LogIn'
+import { Router, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+
+   
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-       
+      hello
+<LogIn/>
+      
+      
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
---
+++ src/LogIn/LogIn.jsx
@@ -0,0 +1,12 @@
+function LogIn(){
+   
+
+    return (
+
+        <div>
+            <input></input> <button>submit</button>
+        </div>
+    )
+}
+
+export default LogIn;
