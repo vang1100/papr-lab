2b1842bd27317e23d2613c2aea04df15bd8d9fd1
Padee Vang <vangpadee11@gmail.com>
2026-05-19 21:00:32 -0500

    created axios get. successful in console

--- src/Blog/Blog.jsx
+++ src/Blog/Blog.jsx
@@ -1,13 +1,58 @@
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Blog() {
 
     const navigate = useNavigate();
+    const [blog, setBlog] = useState('');
+
+    useEffect(() =>{
+
+        fetchBlog();
+        
+    }, []);
+
+     const fetchBlog = () => {
+
+        //axios
+
+        axios.get(`/api/admin`, )
+        .then((response) => {
+            console.log('this is axios get', response.data);
+            setBlog(response.data);
+        })
+        .catch((error) => {
+            console.log('whats the error', error);
+        })
+
+    }
+
+    
 
     return (
         <>This is wehere the Blog exists
-        1. List of blogs
-        2. BUttons to click edit and then directs to editing the blog
+        <br/>
+
+        <ul>
+            <li>
+             List of blogs
+        <li>buttons to click edit and then directs to editing the blog
+            </li>
+            </li>
+        </ul>
+        
+
+<ul>
+    05/19 planning:
+
+        1. display list of blog
+        2. create edit form for blog
+</ul>
+        
+        
+        
+
 
         <button onClick={()=> navigate('/dashboard')}>Dashboard</button>
         </>
